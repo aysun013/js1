815238737d01e347820302434cb1dc7ed9961ece
Aysun Ismayilova <147844360+aysun013@users.noreply.github.com>
2023-11-20 19:56:13 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let email = prompt('Enter your email:');
+
+
+if(email==='admin@gmail.com'){
+    answer = "Siz daxil oldunuz" ;
+ } else{
+    answer = false ;
+ }
+ alert(answer);
+
+let pass = prompt('Enter your password:');
+
+
+if (pass==='12345'){
+    answer = "Password yanlisdir" ;
+} else{
+    answer = false ;
+}
+alert(answer);
